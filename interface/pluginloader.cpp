11e00485eb8378a308b0d51f73fa343b0b9d8c10
Lukas Appelhans <l.appelhans@gmx.de>
2008-10-01 21:31:51 +0200

    Yo

---
+++ interface/pluginloader.cpp
@@ -0,0 +1,48 @@
+#include "pluginloader.h"
+
+#include "plugin.h"
+
+#include <KServiceTypeTrader>
+#include <KDebug>
+
+PluginLoader::PluginLoader(QObject * parent)
+  : QObject(parent)
+{
+}
+
+PluginLoader::~PluginLoader()
+{
+}
+
+void PluginLoader::loadAllPlugins()
+{
+    kDebug() << "Load all plugins";
+    KService::List offers = KServiceTypeTrader::self()->query("Plugintest/Plugin");
+
+    KService::List::const_iterator iter;
+    for(iter = offers.begin(); iter < offers.end(); ++iter)
+    {
+       QString error;
+       KService::Ptr service = *iter;
+
+        KPluginFactory *factory = KPluginLoader(service->library()).factory();
+
+        if (!factory)
+        {
+            //KMessageBox::error(0, i18n("<html><p>KPluginFactory could not load the plugin:<br/><i>%1</i></p></html>",
+              //                         service->library()));
+            kError(5001) << "KPluginFactory could not load the plugin:" << service->library();
+            continue;
+        }
+
+       Plugin *plugin = factory->create<Plugin>(this);
+
+       if (plugin) {
+           kDebug() << "Load plugin:" << service->name();
+           emit pluginCreated(plugin);
+           //guiFactory()->addClient(plugin);
+       } else {
+           kDebug() << error;
+       }
+    }
+}
---
+++ interface/pluginloader.h
@@ -0,0 +1,34 @@
+/* This file is part of the KDE project
+
+   Copyright (C) 2008 Dario Freddi <drf54321@yahoo.it>
+                 2008 Lukas Appelhans <l.appelhans@gmx.de>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+*/
+
+#ifndef PLUGINTESTPLUGINLOADER_H
+#define PLUGINTESTPLUGINLOADER_H
+
+#include <QObject>
+
+#include "plugintest_macros.h"
+
+class Plugin;
+
+class PLUGINTEST_EXPORT PluginLoader : public QObject
+{
+    Q_OBJECT
+    public:
+        PluginLoader(QObject * parent);
+        virtual ~PluginLoader();
+
+        void loadAllPlugins();
+
+    signals:
+        void pluginCreated(Plugin * plugin);
+};
+
+#endif
